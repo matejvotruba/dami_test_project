e53bf09752e33e5dbdb7b356eff521f341572a5c
matejvotruba <ma.votruba@gmail.com>
2022-11-13 19:02:04 +0100

    Add MissionScreen ListView and error screen

--- lib/screens/mission_screen/mission_screen.dart
+++ lib/screens/mission_screen/mission_screen.dart
@@ -1,3 +1,8 @@
+import 'package:dami_test_project/models/rocket_launch.dart';
+import 'package:dami_test_project/styles.dart';
+import 'package:dami_test_project/screens/mission_screen/mission_screen_appbar.dart';
+import 'package:dami_test_project/widgets/dami_material_button.dart';
+import 'package:dami_test_project/widgets/launch_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:dami_test_project/screens/mission_screen/bloc/mission_bloc.dart';
@@ -7,22 +12,71 @@ class MissionScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final appBarHeight = 117 - MediaQuery.of(context).padding.top;
+
     return BlocBuilder<MissionBloc, MissionState>(
       builder: (context, state) {
+        // Show Splash Screen
         if (state is MissionInitialState) {
-          return Container();
+          return Scaffold(
+            backgroundColor: FigmaColors.baliBlue,
+            body: Center(child: Image.asset('images/logo/dami_logo_white.png')),
+          );
         }
 
+        // Show CircularProgressIndicator
         if (state is MissionLoadingState) {
-          return Container();
+          return Scaffold(
+            appBar: MissionScreenAppBar(appBarHeight: appBarHeight),
+            body: const Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
         }
 
+        // Show Error and Try Again button
         if (state is MissionErrorFetchDataState) {
-          return Container();
+          return Scaffold(
+            appBar: MissionScreenAppBar(appBarHeight: appBarHeight),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'DATA DOWNLOAD\n'
+                    'FAILED',
+                    style: FigmaTextStyles.titleLarge.copyWith(
+                      color: FigmaColors.lightBlue,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 28),
+                  DamiMaterialButton(
+                    text: 'TRY AGAIN',
+                    onPressed: () {
+                      context
+                          .read<MissionBloc>()
+                          .add(const MissionTryAgainEvent());
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
         }
 
         if (state is MissionSuccessFetchDataState) {
-          return Container();
+          return Scaffold(
+            appBar: MissionScreenAppBar(appBarHeight: appBarHeight),
+            body: ListView.builder(
+              itemCount: state.launches.length,
+              itemBuilder: (context, i) {
+                final launch = state.launches[i];
+                return LaunchTile(launch: launch);
+              },
+            ),
+          );
         } else {
           return Container();
         }
---
+++ lib/widgets/launch_tile.dart
@@ -0,0 +1,127 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:dami_test_project/models/rocket_launch.dart';
+import 'package:dami_test_project/styles.dart';
+import 'package:flutter/material.dart';
+
+class LaunchTile extends StatelessWidget {
+  const LaunchTile({
+    Key? key,
+    required this.launch,
+  }) : super(key: key);
+
+  final RocketLaunch launch;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      child: Container(
+        decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(
+              Radius.circular(8),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromRGBO(0, 0, 0, 0.15),
+                blurRadius: 15,
+                offset: Offset(0, 4),
+              ),
+            ]),
+        child: Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 14.0),
+                    child: launch.imageUrl != null
+                        ? CachedNetworkImage(
+                            width: 57,
+                            height: 57,
+                            imageUrl: launch.imageUrl!,
+                            progressIndicatorBuilder:
+                                (context, url, downloadProgress) =>
+                                    CircularProgressIndicator(
+                                        value: downloadProgress.progress),
+                            errorWidget: (context, url, error) =>
+                                const Icon(Icons.error),
+                          )
+                        : Image.asset(
+                            'images/damik/damik.png',
+                            width: 57,
+                            height: 57,
+                          ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                width: 206,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      launch.name,
+                      softWrap: false,
+                      overflow: TextOverflow.ellipsis,
+                      style: FigmaTextStyles.titleLarge,
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        text: 'Flight number: ',
+                        style: FigmaTextStyles.labelSmall.copyWith(
+                          fontWeight: FontWeight.w700,
+                          color: FigmaColors.lightBlue,
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: launch.flightNumber.toString(),
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        text: 'Flight date: ',
+                        style: FigmaTextStyles.labelSmall.copyWith(
+                          fontWeight: FontWeight.w700,
+                          color: FigmaColors.lightBlue,
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: launch.date,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: const [
+                    Icon(
+                      Icons.chevron_right,
+                      color: FigmaColors.lightBlue,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
